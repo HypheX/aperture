1cfa1440c875d5f445d409a7074ccb916d27a129
Xelph <fierrosalinas.la@gmail.com>
2023-02-06 08:35:50 -0700

    Fix FEN string

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod search;
 
 
 fn main() {
-    let board = Board::from_fen("6k1/6pp/5p2/8/8/8/8/1R5K w - - 0 1", false).unwrap();
+    let board = Board::from_fen("6k1/5ppp/8/8/8/8/8/1R5K w - - 0 1", false).unwrap();
     let res = search::init_search(&board, 6);
     println!("{0:?} | {1:?}", res.mv, res.evaluation);
 }
